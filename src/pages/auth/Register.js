92a4ee7724f44694dea436e64c9a52afde4c9c5f
mdanisurrahmanronyy <mdanisurrahmanrony50@gmail.com>
2021-02-12 00:55:01 +0600

    lesson12

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Resister = () => {
-    return(
-        <div>
-            <p>Resister</p>
+const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    //
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Resister;
+export default Register;
